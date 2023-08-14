056ad4fa9b6b88b8486bc6ca54eccbfb2b7c38bc
ABCDZF <91682385+ABCDZF@users.noreply.github.com>
2023-08-14 12:48:40 +0530

    Add files via upload

---
+++ assignment 2.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#1
+in python we write code by using " #code " and ''' code '''
+
+comments are mainly 2 types =
+
+Single Line Comment
+Multi Line Comment
+
+#Single Line Comment
+
+'''
+Multi
+Line
+Commeent
+'''
+
+
+# In[ ]:
+
+
+#2
+In Python, variables are used to store and manipulate data. A variable is essentially a name that refers to a value or an object.
+
+Example :
+
+ x = 5
+ x = "Strings"
+
+x is a variable
+
+
+# In[2]:
+
+
+#3
+Using Conversion Functions: convert iterative objects
+
+list(): Converts to a list.
+
+tuple(): Converts to a tuple.
+
+set(): Converts to a set.
+
+dict(): Converts to a dictionary.
+
+
+# In[ ]:
+
+
+#Q4. How do you write and execute a Python script from the command line?
+Ans:
+
+step 1. Create a file with .py extension in a text editor
+
+test.py
+
+step 2. write a code in that file
+
+print("Hello, World!")
+
+step 3. Save that file
+
+Step 4. Open Command Prompt on Windows, Terminal on macOS, or a Linux shell.
+
+step 5. Run that file
+
+python test.py
+
+It will give a output
+
+Hello, World!
+
+
+# In[ ]:
+
+
+#5
+my_list = [1,2,3,4,5]
+sublist = my_list[1:3]
+sublist
+     
+
+
+# In[3]:
+
+
+#6
+In mathematics, a complex number is a number of the form a + bi, where a and b are real numbers and i is the imaginary unit, defined as the square root of -1. The real part a represents the real value of the complex number, while the imaginary part b represents the multiple of i.
+
+in Python the representation of complex number is
+
+  a+bj
+  the 'a' is real part and 'bj' part is imaginary
+
+complex_num =  7 + 8j
+print(complex_num.real)   # output for this line = 7.0
+print(complex_num.imag)   # output for this line = 8.0
+     
+
+
+# In[ ]:
+
+
+#7
+Age = 25 # Age is variable , 25 is its value and the datatype is Int
+
+
+# In[ ]:
+
+
+#8
+price = 9.99
+type(price)
+#output-float
+
+
+# In[ ]:
+
+
+#9
+name ="chandan gupta"
+print(name)
+
+
+# In[ ]:
+
+
+#10
+string = "Hello, World!"
+substring= string[7:12]
+substring
+     
+
+
+# In[5]:
+
+
+#11
+is_student = True  # I am an student so, its True
+is_student
+
